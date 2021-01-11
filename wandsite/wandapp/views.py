d8bf7876df1ff4ba4af5c5511489d6ac7dd9c884
green-pea-ai <yachtboatfor@gmail.com>
2021-01-11 22:50:19 +0900

    22:49, render()를 사용해 더 간단한 template의 표현방법

--- wandsite/wandapp/views.py
+++ wandsite/wandapp/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 from .models import Question
 from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 # def index(request):
@@ -12,11 +13,10 @@ from django.template import loader
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('wandapp/index.html')
 	context = {
 		'latest_question_list': latest_question_list,
 	}
-	return HttpResponse(template.render(context, request))	
+	return render(request, 'wandapp/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
