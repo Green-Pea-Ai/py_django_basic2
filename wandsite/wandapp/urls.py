c40be374461db61ec22fc95c6b88dad0002740c7
green-pea-ai <yachtboatfor@gmail.com>
2021-01-11 22:17:04 +0900

    210111 Mon 22:16, views.py에 template 코드 적용 전

--- wandsite/wandapp/urls.py
+++ wandsite/wandapp/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	# ex: /wandapp/
 	path('', views.index, name='index'),
+
+	# ex: /wandapp/5/
+	path('<int:question_id>/', views.detail, name='detail'),
+
+	# ex: /wandapp/5/results/
+	path('<int:question_id>/results/', views.results, name='results'),
+
+	# ex: /wandapp/5/vote/
+	path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
