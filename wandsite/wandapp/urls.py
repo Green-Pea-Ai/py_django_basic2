a97f1e98f9265573852a5de36196035ab0a95f2a
green-pea-ai <yachtboatfor@gmail.com>
2021-01-12 14:38:14 +0900

    14:37, template 부분의 url 작성하는 방법 관련 내용.

--- wandsite/wandapp/urls.py
+++ wandsite/wandapp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'wandapp'
 urlpatterns = [
 	# ex: /wandapp/
 	path('', views.index, name='index'),
 
 	# ex: /wandapp/5/
+	# the 'name' value as called by the {% url %} template tag
 	path('<int:question_id>/', views.detail, name='detail'),
 
 	# ex: /wandapp/5/results/
