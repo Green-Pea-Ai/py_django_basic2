e8e6de11b0c2e1471607d8cd9552379be3b0c807
green-pea-ai <yachtboatfor@gmail.com>
2021-01-13 23:40:45 +0900

    23:37, admin form customizing 실습

--- wandsite/wandapp/admin.py
+++ wandsite/wandapp/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 
 from .models import Question
 
+from .models import Choice
+
 # Register your models here.
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+
+	# admin 페이지의 컬럼 순서를 사용자 설정할 수 있다.
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
